c08e2ae2eed0a855ac0bc009ac647d098cdcb273
Fiona-zy <1614735214@qq.com>
2020-11-24 10:05:10 +0800

    注册登录页面

---
+++ assets/js/base.js
@@ -0,0 +1,5 @@
+//ajax基本配置
+$.ajaxPrefilter(function (options) {
+    options.url = 'http://ajax.frontend.itheima.net' + options.url;
+    console.log(options.url);
+});
---
+++ assets/js/login.js
@@ -0,0 +1,81 @@
+$(function () {
+    //跳转切换
+    $('.gotoRegi').click(function () {
+        $('.loginBox').hide()
+        $('.regiBox').show()
+    });
+    $('.gotoLogin').click(function () {
+        $('.loginBox').show()
+        $('.regiBox').hide()
+    });
+    //校验
+    let form = layui.form;
+    let layer = layui.layer;
+    form.verify({
+        //我们既支持上述函数式的方式，也支持下述数组的形式
+        //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
+        pass: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+
+        repass:function (value,item) {
+            // console.log(value, item);
+            let pwd = $(".regiBox input[name=password]").val();
+            if (value !== pwd) {
+                return "两次输入的密码不一致!";
+            }
+        }
+
+    });
+    
+    //注册ajax
+    $('#regiForm').on('submit', function (e) {
+        e.preventDefault();
+
+        let data = $(this).serialize();
+        $.ajax({
+            type: 'POST',
+            url: 'http://ajax.frontend.itheima.net/api/reguser',
+            data: data,
+            success: function (res) {
+                // console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                };
+                layer.msg('注册成功');
+                $('.gotoLogin').click();
+
+            }
+        })
+    });
+    //登录
+    $('#loginForm').on('submit',function (e) {
+        e.preventDefault();
+        let data = $(this).serialize();
+        $.ajax({
+            type: 'POST',
+            url: '/api/login',
+            data: data,
+            success:function (res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg('登录失败')
+                };
+                localStorage.setItem('token', res.token);
+                layer.msg(
+                    '登录成功，即将去后台主页',
+                    {
+                        time:2000,
+                    },//2秒后关闭
+                    function () {
+                         //2秒后关闭后做的事情
+                        location.href = 'index.html';
+                    }
+                )
+            }
+        })
+    })
+
+
+
+
+
+});
